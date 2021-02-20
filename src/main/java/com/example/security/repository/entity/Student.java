45b4b16a4c8073470925b9acdc5ef4f0255dee41
Paweł Nowak <kontakt.nowakpawel@gmail.com>
2021-02-21 00:28:44 +0100

    Load dummy data and run getStudent method

--- src/main/java/com/example/security/repository/entity/Student.java
+++ src/main/java/com/example/security/repository/entity/Student.java
@@ -1,4 +1,19 @@
 package com.example.security.repository.entity;
 
 public class Student {
+    private final Integer StudentId;
+    private final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        StudentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return StudentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
 }
--- src/main/java/com/example/security/web/controller/StudentController.java
+++ src/main/java/com/example/security/web/controller/StudentController.java
@@ -6,12 +6,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
+    private static final Logger logger = Logger.getLogger(StudentController.class.getName());
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Paweł Nowak"),
+            new Student(2,"James Bond"),
+            new Student(3,"John Doe")
+    );
 
-    @GetMapping("/studentId")
+    @GetMapping( "/{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        logger.info("INFO:  Get specific student with id " + studentId);
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst() //TODO: create own exception
+                .orElseThrow(() -> new IllegalStateException(("Student " + studentId + " does not exists!")));
+
 
     }
 }
